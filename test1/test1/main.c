540b8563207c762ba7440addb9bbaef9e8158446
fxiao001 <739193669@qq.com>
2022-03-05 22:25:15 +0800

    First Commit

---
+++ test1/test1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  test1
+//
+//  Created by 冯晓 on 2022/3/5.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
